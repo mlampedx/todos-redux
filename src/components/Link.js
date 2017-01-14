23ff2a389a39cb15256d4aa5e44dbadb0346f101
mlampedx <mlampesd@gmail.com>
2017-01-14 12:36:24 -0800

    Changed links to flat buttons

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,26 +1,18 @@
 import React, { PropTypes } from 'react';
+import FlatButton from 'material-ui/FlatButton';
 
 const Link = ({ active, children, onClick }) => {
-  if (active) {
-    return <span>{children}</span>
-  }
 
   return (
-    <a href="#"
-      onClick={e => {
+    <FlatButton
+      label = {children}
+      onClick = {e => {
         e.preventDefault();
         onClick();
       }}
-    >
-      {children}
-    </a>
+      primary = {active}
+    />
   )
-}
-
-Link.propTypes = {
-  active: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired
 }
 
 export default Link;
