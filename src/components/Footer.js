967d8f5330817f0ac2b9f56b86276b901ef44754
mlampedx <mlampesd@gmail.com>
2017-01-14 12:36:02 -0800

    Centered filter links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,19 @@ import React, { PropTypes } from 'react';
 import FilterLink from '../containers/FilterLink';
 
 const Footer = () => (
-  <p>
-    Show:
-    {' '}
-    <FilterLink filter='SHOW_ALL'>
-      All
-    </FilterLink>
-    {', '}
-    <FilterLink filter='SHOW_ACTIVE'>
-      Active
-    </FilterLink>
-    {', '}
-    <FilterLink filter='SHOW_COMPLETED'>
-      Completed
-    </FilterLink>
+  <p className = 'centered-elem'>
+    <span>SHOW:
+     {' '}
+      <FilterLink filter='SHOW_ALL'>
+        All
+      </FilterLink>
+      <FilterLink filter='SHOW_ACTIVE'>
+        Active
+      </FilterLink>
+      <FilterLink filter='SHOW_COMPLETED'>
+        Completed
+      </FilterLink>
+    </span>
   </p>
 )
 
