bdabc2c823b65c3a4a9355b3a68523ce6da16eb6
mlampedx <mlampesd@gmail.com>
2017-01-14 12:38:33 -0800

    Refactored undo/redo button design

--- src/components/UndoRedoPres.js
+++ src/components/UndoRedoPres.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+// import FontIcon from 'material-ui/FontIcon';
 
 const UndoRedoPres = ({ canUndo, canRedo, onUndo, onRedo }) => (
-  <p>
-    <button onClick = { onUndo } disabled = { !canUndo }>
-    Undo
-    </button>
-    <button onClick = { onRedo } disabled = { !canRedo }>
-    Redo
-    </button>
+  <p className = 'centered-elem'>
+    <FlatButton
+      label = 'Undo'
+      secondary = { true }
+      onClick = { onUndo }
+      disabled = { !canUndo }
+      // icon = { <FontIcon className = '' /> }
+    />
+    <FlatButton
+      label = 'Redo'
+      primary = { true }
+      onClick = { onRedo }
+      disabled = { !canRedo }
+      // icon = { <FontIcon className = '' /> }
+    />
   </p>
 )
 
