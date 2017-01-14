01d45cbe344d8f6924bcbb048ee73cd6cffdf2a0
mlampedx <mlampesd@gmail.com>
2017-01-14 12:35:29 -0800

    Added header bar

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,19 @@ import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 import UndoRedo from '../containers/UndoRedo';
+import AppBar from 'material-ui/AppBar';
 
 const App = () => (
   <div>
+    <AppBar
+      className = 'app-bar'
+      title = { <span className = 'centered-elem'>TASK MANAGER</span> }
+      showMenuIconButton = { false }
+    />
     <AddTodo />
-    <VisibleTodoList />
     <Footer />
     <UndoRedo />
+    <VisibleTodoList />
   </div>
 )
 
